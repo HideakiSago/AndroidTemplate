94db2ccf4a0272432934165224eafcbd2a6e9339
hideaki.sago <hideaki.sago@do-until.jp>
2016-02-16 18:45:51 +0900

    navigationの項目を実際に使うものに変更

--- app/src/develop/java/jp/hideakisago/androidtemplate/viewcontrollers/menu/DevelopMenuActivity.java
+++ app/src/develop/java/jp/hideakisago/androidtemplate/viewcontrollers/menu/DevelopMenuActivity.java
@@ -2,6 +2,7 @@ package jp.hideakisago.androidtemplate.viewcontrollers.menu;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -73,25 +74,37 @@ public class DevelopMenuActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        final Class<?> fragmentClass;
+
+        switch (id) {
+            case R.id.nav_screens:
+                fragmentClass = ScreenListFragment.class;
+                break;
+            case R.id.nav_rest_apis:
+                fragmentClass = ScreenListFragment.class;
+                break;
+            case R.id.nav_samples:
+                fragmentClass = ScreenListFragment.class;
+                break;
+            case R.id.nav_others:
+                fragmentClass = ScreenListFragment.class;
+                break;
+            default:
+                throw new RuntimeException("Unknown ID.");
+        }
+
+        FragmentManager fm = getSupportFragmentManager();
+        if (fragmentClass.equals(fm.findFragmentById(R.id.content_frame).getClass())) {
+            return true;
+        }
+
         return true;
     }
 }
